c43d1adc1ffbe27dc0ad41859b72d4605fc80994
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-02-16 15:42:55 +0330

    feat: add dynamic module

--- src/logger.module.ts
+++ src/logger.module.ts
@@ -1,39 +1,49 @@
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, DynamicModule } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston'
 import 'winston-daily-rotate-file';
+import { DailyRotateFile } from 'winston/lib/winston/transports';
+
+interface LoggerModuleOptions {
+  logPath: string
+}
 
 @Global()
-@Module({
-  imports: [
-    WinstonModule.forRoot({
-      level: "silly",
-      transports: [
-        new winston.transports.Console({
+@Module({})
+export class LoggerModule {
+  static forRoot(options: LoggerModuleOptions): DynamicModule {
+    return {
+      module: LoggerModule,
+      imports: [
+        WinstonModule.forRoot({
           level: "silly",
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.colorize(),
-            winston.format.printf(({ timestamp, level, message }) => {
-              return `[${timestamp}] ${level}: ${message}`
+          transports: [
+            new winston.transports.Console({
+              level: "silly",
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.colorize(),
+                winston.format.printf(({ timestamp, level, message }) => {
+                  return `[${timestamp}] ${level}: ${message}`;
+                })
+              )
+            }),
+            new DailyRotateFile({
+              level: "silly",
+              filename: `${options.logPath}/application-%DATE%.log`,
+              datePattern: 'YYYY-MM-DD',
+              zippedArchive: true,
+              maxSize: '10m',
+              maxFiles: '14d',
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json()
+              )
             })
-          )
-        }),
-        new winston.transports.DailyRotateFile({
-          level: "silly",
-          filename: 'logs/application-%DATE%.log',
-          datePattern: 'YYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '10m',
-          maxFiles: '14d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json()
-          )
+          ]
         })
-      ]
-    })
-  ],
-  exports: [WinstonModule]
-})
-export class LoggerModule { }
+      ],
+      exports: [WinstonModule]
+    };
+  }
+}
